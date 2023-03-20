593a3eb94aa4c9d044b5e65e0e860f4edecbb0ac
canon20paul <canon20paul@gmail.com>
2023-03-20 18:49:03 +0300

    deployment2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/E-commerce-React-Redux",
+  base: "/E-commerce-React-Redux/",
   plugins: [react()],
 })
