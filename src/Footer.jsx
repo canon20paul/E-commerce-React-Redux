07f9085db3bf0dbc131e8e58958eb40397be94f4
canon20paul <canon20paul@gmail.com>
2023-03-20 16:39:37 +0300

    Footerr

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+      <div style={{backgroundColor:'blue',color:'white', marginBottom:'0'}}>
+          <p>Designed and Developed by</p>    
+          <p>Paul Malongo</p>
+
+      </div>
+  )
+}
